b3b201ee6758b6ec61dc04460b872f6d3d204ebb
Alejandro Keller <alejandro.keller@fhnw.ch>
2019-03-21 14:21:45 +0100

    correction to FATCAT-scripts directory name

--- launchers/launcher_analysis_mode.sh
+++ launchers/launcher_analysis_mode.sh
@@ -2,7 +2,7 @@
 # launcher_analysis_mode.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
-cd FATCAT-scripts/commands
+cd /FATCAT-scripts/commands
 python fatcat_select_mode.py --analysis
 #python tca_commands.py --set-flow 02
 cd /
