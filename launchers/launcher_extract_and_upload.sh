7b6cd3d24abb94969a756d9f39da0f5111b37e20
root <alejandro.keller@fhnw.ch>
2019-03-21 11:01:51 +0000

    creates now a summary file

--- launchers/launcher_extract_and_upload.sh
+++ launchers/launcher_extract_and_upload.sh
@@ -4,7 +4,8 @@
 cd /GAW-Instrument
 today=`date '+%Y%m%d'`
 filename="/home/pi/fatcat-files/data/$today-Ambient-Denuder.dat"
+file2="/home/pi/fatcat-files/data/summaries/$today-120s.dat"
 python export-day.py > $filename
 cd /FATCAT-scripts/
-python /FATCAT-scripts/fatcat_integrate_co2.10min.py --intlength 120 --upload --last $filename
+python /FATCAT-scripts/fatcat_integrate_co2.10min.py --intlength 120 --no-header --upload --last $filename >> $file2
 cd /
