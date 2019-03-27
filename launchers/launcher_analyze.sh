4cef193797ca0937306e3371bb48e3b9c2a4087b
Alejandro Keller <alejandro.keller@fhnw.ch>
2019-03-27 09:44:42 +0100

    modified launcher analize

--- launchers/launcher_analyze.sh
+++ launchers/launcher_analyze.sh
@@ -4,5 +4,5 @@
 cd /FATCAT-scripts/
 today=`date '+%Y%m%d'`
 file2="/home/pi/fatcat-files/data/summaries/$today-120s.dat"
-python /FATCAT-scripts/fatcat_integrate_co2.10min.py --intlength 120 --last $(ls -t /home/pi/fatcat-files/data/* | head -1) >> $file2
+python /FATCAT-scripts/fatcat_integrate_co2.10min.py --intlength 120 --no-header --last $(ls -t /home/pi/fatcat-files/data/* | head -1) >> $file2
 cd /
