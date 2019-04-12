afe3dca630c466ff9f8ef2f78312631478c8662f
Alejandro Keller <41949530+alejandrokeller@users.noreply.github.com>
2019-04-12 14:11:34 +0200

    Update fatcat_integrate_co2.10min.py

--- fatcat_integrate_co2.10min.py
+++ fatcat_integrate_co2.10min.py
@@ -42,7 +42,7 @@ class Datafile(object):
         self.lines2skip = 5                         # skip file headers plus first two lines due to uknown errors
         self.skipedlines = []
 
-        print >>sys.stderr, 'Counting events in datafile "{0}"}.format(self.datafile)
+        print >>sys.stderr, 'Counting events in datafile "{0}"'.format(self.datafile)
         self.numSamples, events = self.countAndFetchEvents(lines_to_skip = self.lines2skip)
         if events[-1] >= self.numSamples:
            events = events[:len(events)-1]
