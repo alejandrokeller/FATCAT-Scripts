0376116cc4c222cd0d943b01fc7ca22b4a6cac45
Alejandro Keller <alejandro.keller@fhnw.ch>
2019-04-17 16:54:28 +0200

    added switch pump launchers

---
+++ launchers/launcher_external_pump.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher_sample_mode.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /FATCAT-scripts/commands
+python tca_commands.py --pump off
+python tca_commands.py --extpump on
+cd /
