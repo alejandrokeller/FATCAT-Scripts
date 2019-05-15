47f7ceff6a39f393316e10990107ab63b3f55a6a
Alejandro Keller <41949530+alejandrokeller@users.noreply.github.com>
2019-05-15 16:06:57 +0200

    Update plot_event.py

--- extras/plot_event.py
+++ extras/plot_event.py
@@ -5,14 +5,24 @@ import plotly.figure_factory as FF
 import numpy as np
 import pandas as pd
 
-sys.path.append('../')
+class Datafile(object):
+    def __init__(self, datafile, output_path = 'data/events/graph/'): # datafile is a valid filepointer
+        
+        #init data structure
+        self.datastring = ""
+        self.datafile   = datafile.name
+        self.csvfile    = datafile
+        self.outputDir   = output_path
+        self.date       = time.strftime("%Y-%m-%d")
+        self.internname = self.csvfile.readline().rstrip('\n') # first line contains the original filename
+        self.rawdata    = self.csvfile.readline().rstrip('\n') # second line points to raw data
 
-def load_df(inpath, outpath):
-    df = pd.read_csv(path)
-
-    sample_data_table = FF.create_table(df.head())
-    filename = outpath + 'sample-data-table'
-    py.iplot(sample_data_table, filename=filename)
+    def load_df():
+        self.df = pd.read_csv(self.eventDir, header=[2,3])
+        
+        sample_data_table = FF.create_table(df.head())
+        filename = self.outputDir + 'sample-data-table'
+        py.iplot(sample_data_table, filename=filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Graph generator for fatcat event files.')
@@ -31,9 +41,9 @@ if __name__ == "__main__":
         output_path = eval(config['GENERAL_SETTINGS']['EVENTS_PATH']) + '/graph/'
     else:
         events_path = '~/fatcat-files/data/events/'  # if ini file cannot be found
-        output_path = '~/fatcat-files/data/events/graph/'
+        output_path = events_path + 'graph/'
         print >>sys.stderr, 'Could not find the configuration file {0}'.format(config_file)
 
     with args.datafile as file:
-        path = event_path + file
-        load_df(inpath = path, outpath = output_path)
+        mydata = Datafile(file, output_path = output_path)
+        mydata.load_df()
