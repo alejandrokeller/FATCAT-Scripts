17fe685e04ddd570d557b3ce86f04f471ffd32f3
Alejandro Keller <41949530+alejandrokeller@users.noreply.github.com>
2019-07-01 16:23:46 +0200

    port address now refers to instrument

--- port_address.py
+++ port_address.py
@@ -6,16 +6,10 @@ import serial.tools.list_ports
 import sys, os, configparser
 
 sys.path.append('./extras/')
-from tca import serial_ports
+from instrument import instrument
 
 # READ ini file
 config_file = 'config.ini'
-if os.path.exists(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    port_name = eval(config['SERIAL_SETTINGS']['SERIAL_PORT_DESCRIPTION'])
-else:
-    port_name = 'nano-TD'  # if ini file cannot be found
-    print >>sys.stderr, 'Could not find the configuration file {0}'.format(config_file)
+device = instrument(config_file)
 
-print serial_ports(port_name)
+print device.serial_ports(port_name)
