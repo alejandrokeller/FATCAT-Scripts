d977269e1b3128f678e6e31cf90467b0f22c8854
Alejandro Keller <41949530+alejandrokeller@users.noreply.github.com>
2019-07-01 21:01:16 +0200

    Update tca_oven_off.py

--- commands/tca_oven_off.py
+++ commands/tca_oven_off.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-import time, sys
+import sys
 
 sys.path.append('../extras/')
 from instrument import instrument
 
 config_file = '../config.ini'
 device = instrument(config_file)
-
-timestamp = time.strftime("%y.%m.%d-%H:%M:%S ")
-print timestamp + "Turning induction oven off..."
-
+device.log_message("COMMANDS", "Turning induction oven off...")
 device.send_commands(['O0000'], open_port = True)
