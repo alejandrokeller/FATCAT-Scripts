7e9c6d497d097387eecbdf904b71b9a8dc47324b
Alejandro Keller <alejandro.keller@fhnw.ch>
2019-09-24 10:55:53 +0200

    script detects stdout and decides if graphs are interactive

--- extras/day_summary.py
+++ extras/day_summary.py
@@ -11,7 +11,11 @@ import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 
 import matplotlib
-matplotlib.use('Agg') # runs graphs silently without user interaction or from cronjobs
+if not os.isatty(sys.stdout.fileno()):
+    # runs graphs silently without user interaction
+    # used if the script is run as cronjob or if
+    # stdout has been redirected
+    matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 #print(plt.style.available)
