d536c5a3bed6633e74c91777ee2e776052ba8477
Alejandro Keller <alejandro.keller@fhnw.ch>
2019-10-03 15:46:42 +0200

    some cleanning of files from fatcat

--- launchers/launcher_analysis_mode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# launcher_analysis_mode.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /FATCAT-scripts/commands
-./fatcat_select_mode.py --analysis
-cd /
--- launchers/launcher_sample_mode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# launcher_sample_mode.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /FATCAT-scripts/commands
-./fatcat_select_mode.py --sample
-cd /
