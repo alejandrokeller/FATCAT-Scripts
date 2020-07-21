50e8f47269410a39e63f67d06dfb758dc08054fc
alejandro keller <alejandro.keller*fhnw.ch>
2020-07-21 13:04:30 +0200

    incremented range of s2 parameter

--- extras/fit.py
+++ extras/fit.py
@@ -14,7 +14,7 @@ def my_fit(xdata, ydata, p0 = (10., 18., 3., 8., 28., 7., 6., 53., 8. )):
     nparameters = 3
     
     fitResult, ier = curve_fit( my_triple_peak, xdata, ydata, p0=p0, 
-                                bounds=((0, 0, 0, 0, 20, 0, 0, 40, 0),
+                                bounds=((0, 0, 0, 0, 20, 0, 0, 37, 0),
                                         (np.inf, 20, np.inf, np.inf, 40, np.inf, np.inf, 60, 20)) )
     perr = np.sqrt(np.diag(ier))
     fit_coeff = []
