5156df01699fa4dab8827f59a8a134905511ee81
alejandro keller <alejandro.keller@fhnw.ch>
2021-11-22 13:19:37 +0000

    modify launchers for JFJ

--- launchers/launcher_zeroair.sh
+++ launchers/launcher_zeroair.sh
@@ -3,6 +3,5 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /FATCAT-scripts/commands
-./fatcat_select_mode.py --standby --analysis
-./serial_commands.py --valve off --extpump off
+./fatcat_select_mode.py --analysis
 cd /
