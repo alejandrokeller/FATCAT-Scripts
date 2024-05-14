9e352dfce84ce567dccaba5abf6303759a56309b
Alejandro Keller <alejandro.keller@fhnw.ch>
2024-05-14 14:40:52 +0200

    windows compatibilita issue

--- extras/plot_event.py
+++ extras/plot_event.py
@@ -838,7 +838,8 @@ def create_baseline_file(files, baseline_path, baseline_file, summary_path, tmax
 
     print(stats_df.head())
     box_plot(x = results.summary['date']+' '+results.summary['time'], y = results.summary['tc'],
-             title = 'Baseline data', units = r'$\mu$g-C', filename = baseline_file, path = baseline_path)
+             title = 'Baseline data', units = r'$\mu$g-C', filename = baseline_file, path = baseline_path,
+             date_format='%Y-%m-%d %H:%M:%S')
         
     return filename
 
